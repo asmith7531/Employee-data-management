8d2624ac574b3409e994da88570476dbb0d1d58a
Austin Smith <austinsmith7531@gmail.com>
2019-03-30 10:19:07 -0400

    cdn

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//initializing firebase
+var config = {
+  apiKey: "AIzaSyB14F_mVqpnnkeiHeSJ0ugPi94B1oqyAPk",
+  authDomain: "employee-database-c46ff.firebaseapp.com",
+  databaseURL: "https://employee-database-c46ff.firebaseio.com",
+  projectId: "employee-database-c46ff",
+  storageBucket: "",
+  messagingSenderId: "176647660987"
+};
+firebase.initializeApp(config);
